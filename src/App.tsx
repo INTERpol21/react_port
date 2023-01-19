44f70dde80587c694f23718596dcad871b7c061e
Anton Zhuravlev <79113636+INTERpol21@users.noreply.github.com>
2023-01-20 00:12:18 +0600

    Img

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import "./styles/main.css";
 import NavBar from "./component/navbar/NavBar";
 import Footer from "./component/footer/Footer";
 import Home from "./pages/Home";
+import Project from "./pages/Project";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Home />
+      {/* <Home /> */}
+      <Project/>
       <Footer />
     </div>
   );
 }
 
 export default App;
+       
