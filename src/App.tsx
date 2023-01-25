eaf6a0263e2f96df263287fbfa20b675dc46c1cc
Anton Zhuravlev <79113636+INTERpol21@users.noreply.github.com>
2023-01-25 21:40:00 +0600

    modified:   src/App.tsx 	new file:   src/helpers/projectList.tsx 	renamed:    src/img/projects/PorrtfolioReact.png -> src/img/projects/PortfolioReact.png 	new file:   src/img/projects/TodoV1.1.png 	new file:   src/pages/Contacts.tsx 	deleted:    src/pages/Project.tsx 	new file:   src/pages/Projects.tsx 	new file:   src/project/Project.tsx 	new file:   src/project/slyle.css 	modified:   src/styles/main.css

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import "./styles/main.css";
 import NavBar from "./component/navbar/NavBar";
 import Footer from "./component/footer/Footer";
 // import Home from "./pages/Home";
-import Project from "./pages/Project";
+// import Project from "./pages/Project";
+import Contacts from "./pages/Contacts";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       {/* <Home /> */}
-      <Project/>
+      {/* <Project/> */}
+      <Contacts/>
       <Footer />
     </div>
   );
---
+++ src/helpers/projectList.tsx
@@ -0,0 +1,103 @@
+import project1 from "../img/projects/activebox.png";
+import project2 from "../img/projects/Blog.png";
+import project3 from "../img/projects/ideas.png";
+import project4 from "../img/projects/learnReact.png";
+import project5 from "../img/projects/mogo.png";
+import project6 from "../img/projects/PortfolioReact.png";
+import project7 from "../img/projects/portfolio.png";
+import project8 from "../img/projects/TodoV1.png";
+import project9 from "../img/projects/TodoV1.1.png";
+import project10 from "../img/projects/TrelloClone.png";
+import project11 from "../img/projects/upavhouse.png";
+import project12 from "../img/projects/upavhouse2.png";
+import project13 from "../img/projects/leverans.png";
+import Project from "../project/Project";
+
+export const projects = [
+  {
+    title: "PortfolioReact",
+    skills: "React, Css, HTML, TS  ",
+    img: { project6 },
+    gitHubLink: "https://github.com/INTERpol21/Project/tree/main/Active",
+    description:
+      "Один из первых пэт-проектов,проработка верстки, работа с Figma и Adobe Photoshop",
+  },
+  {
+    title: "ActiveBox",
+    skills: "Css, HTML, JS, БЭМ, Адаптив,Figma ",
+    img: { project1 },
+    gitHubLink: "https://github.com/INTERpol21/Project/tree/main/Active",
+    description:
+      "Один из первых пэт-проектов,проработка верстки, работа с Figma и Adobe Photoshop",
+  },
+  {
+    title: "ActiveBox",
+    skills: "Css, HTML, JS, БЭМ, Адаптив,Figma ",
+    img: { project1 },
+    gitHubLink: "https://github.com/INTERpol21/Project/tree/main/Active",
+    description:
+      "Один из первых пэт-проектов,проработка верстки, работа с Figma и Adobe Photoshop",
+  },
+  {
+    title: "ActiveBox",
+    skills: "Css, HTML, JS, БЭМ, Адаптив,Figma ",
+    img: { project1 },
+    gitHubLink: "https://github.com/INTERpol21/Project/tree/main/Active",
+    description:
+      "Один из первых пэт-проектов,проработка верстки, работа с Figma и Adobe Photoshop",
+  },
+  {
+    title: "ActiveBox",
+    skills: "Css, HTML, JS, БЭМ, Адаптив,Figma ",
+    img: { project1 },
+    gitHubLink: "https://github.com/INTERpol21/Project/tree/main/Active",
+    description:
+      "Один из первых пэт-проектов,проработка верстки, работа с Figma и Adobe Photoshop",
+  },
+  {
+    title: "TodoV1",
+    skills: "React,Redux,MUI,TS",
+    img: { project8, project9 },
+    gitHubLink: "https://github.com/INTERpol21/Todo-React-Redux-Mui",
+    description:
+      "Отличный пример для первого знакомства с React(по мимо базовых шашек из оф. документации), разделение на слайсы и state с помощью Redux, Типизация и знакомство с прекрасным инструментом TypeScript, впервые использовались готовые компоненты из MUI P.S Можно сказать первое знакомство с реальным миром Frontend",
+  },
+  {
+    title: "Blog",
+    skills: "SCSS, HTML, JS, БЭМ, Адаптив, Figma, Gulp, yarn",
+    img: { project2 },
+    gitHubLink:
+      "https://github.com/INTERpol21/Multipager/tree/master/my-project",
+    description:
+      "Многостраничный лендинг, сборная солянка из предыдущих проектов, интересный дизайн и проработка с Gulp ",
+  },
+  {
+    title: "Portfolio",
+    skills: "Css, HTML, JS, БЭМ, Адаптив,Figma ",
+    img: { project7 },
+    gitHubLink: "https://github.com/INTERpol21/Project/tree/main/skills",
+    description: "Интересный дизайн, красивый адаптив, валидация ",
+  },
+  {
+    title: "Mogo",
+    skills: "Css, HTML, JS, БЭМ, Адаптив,Figma ",
+    img: { project5 },
+    gitHubLink: "https://github.com/INTERpol21/Project/tree/main/mogo%20проект",
+    description: "Интересный дизайн, красивый адаптив, валидация ",
+  },
+  {
+    title: "Ideas",
+    skills: "Css, HTML, JS, БЭМ, Адаптив, Figma",
+    img: { project3 },
+    gitHubLink: "https://github.com/INTERpol21/Project/tree/main/ideas",
+    description: "Интересная работа с красивой и удобной каруселью, валидация",
+  },
+  {
+    title: "ActiveBox",
+    skills: "Css, HTML, JS, БЭМ, Адаптив, Figma",
+    img: { project1 },
+    gitHubLink: "https://github.com/INTERpol21/Project/tree/main/Active",
+    description:
+      "Один из первых пэт-проектов,проработка верстки, работа с Figma и Adobe Photoshop",
+  },
+];
